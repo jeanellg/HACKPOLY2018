c93e3c1d17be3a52fbeaf7369eafc837fc851db0
Anne-Claire Fouchier <Anne-ClaireFouchier@MacBook-Pro-de-Anne-Claire.local>
2018-02-04 12:05:19 -0800

    change error in KCdb.dsl

--- KCdb.sql
+++ KCdb.sql
@@ -17,12 +17,12 @@ INSERT INTO deeds (name) VALUES ('Adopt an abandoned animal');
 
 INSERT INTO locations (country, city, lat, lng) VALUES ('United States', 'Irvine', 33.684567, -117.826505);
 INSERT INTO locations (country, city, lat, lng) VALUES ('France', 'Ruy-Montceau', 45.587724, 5.318858);
-lngINSERT INTO locations (country, city, lat, lgn) VALUES ('United States', 'Garden Grove', 33.774269, -117.937995);
+INSERT INTO locations (country, city, lat, lgn) VALUES ('United States', 'Garden Grove', 33.774269, -117.937995);
 INSERT INTO locations (country, city, lat, lgn) VALUES ('United States', 'Los Angeles', 34.052234, -118.243685);
 INSERT INTO locations (country, city, lat, lgn) VALUES ('Germany', 'Berlin', 52.520007, 13.404954);
 INSERT INTO locations (country, city, lat, lgn) VALUES ('United Kingdom', 'London', 51.507351, -0.127758);
 
-INSERT INTO done_deed (participantId, deedId, locationID) VALUES (1, 5, 2); 
+INSERT INTO done_deed (participantId, deedId, locationID) VALUES (1, 5, 2);
 INSERT INTO done_deed (participantId, deedId, locationID) VALUES (2, 1, 6);
 INSERT INTO done_deed (participantId, deedId, locationID) VALUES (3, 7, 1);
 INSERT INTO done_deed (participantId, deedId, locationID) VALUES (4, 4, 7);
@@ -34,5 +34,3 @@ INSERT INTO friendships(participantId, friendId) VALUES (1, 4);
 INSERT INTO friendships(participantId, friendId) VALUES (2, 3);
 INSERT INTO friendships(participantId, friendId) VALUES (2, 4);
 INSERT INTO friendships(participantId, friendId) VALUES (3, 4);
-
-
